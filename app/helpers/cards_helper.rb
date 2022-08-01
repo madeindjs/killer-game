e834dd786b1fd9890a1b58e32590c58af139ba8d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-08-02 00:15:20 +0200

    generate qr code

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,6 @@
 module CardsHelper
+  def card_qr_code card
+    url = game_card_url(game_id: card.game_id, id: card.token)
+    RQRCode::QRCode.new(url).as_svg({viewbox: true}).html_safe
+  end
 end
