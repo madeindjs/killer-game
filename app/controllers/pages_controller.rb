a428979c16c00907edc08ce7ae1369503a273d1d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-01-29 01:02:43 +0100

    add help

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,42 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def help
+    players = [
+      Player.new(name: "Bob"),
+      Player.new(name: "Alice"),
+      Player.new(name: "Luc"),
+      Player.new(name: "Fred"),
+    ]
+
+    actions = I18n.t('game.default_fields.actions')
+
+    cards = [
+      Card.new(player: players[0], target: players[1], action: actions[0]),
+      Card.new(player: players[1], target: players[2], action: actions[1]),
+      Card.new(player: players[2], target: players[3], action: actions[2]),
+      Card.new(player: players[3], target: players[0], action: actions[3]),
+    ]
+    cards.map{|card| card.done_at = Time.now}
+
+
+    @exemple1 = [
+      { player: players[0], cards: [], current: cards[0] },
+      { player: players[1], cards: [], current: cards[1] },
+      { player: players[2], cards: [], current: cards[2] },
+      { player: players[3], cards: [], current: cards[3] },
+    ]
+
+    @exemple2 = [
+      { player: players[0], cards: [], current: cards[0] },
+      { player: players[1], cards: [cards[1]], current: cards[2] },
+      { player: players[3], cards: [], current: cards[3] },
+    ]
+
+    @exemple3 = [
+      { player: players[0], cards: [cards[0], cards[1]], current: cards[2] },
+      { player: players[3], cards: [], current: cards[3] },
+    ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     resources :cards, only: %i[show index edit update]
     resources :players
   end
+
+  # pages
   get 'pages/home'
+  get 'help', to: 'pages#help', as: :help
+
   get 'games/:token/dashboard', to: 'games#dashboard', as: :game_dashboard
   get 'cards/:token', to: 'cards#show', as: :public_card
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
