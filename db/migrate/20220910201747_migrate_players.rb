822f93f120951d9329807cf0e8fde089fb67d1f4
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-02-02 21:20:26 +0100

    fix migration

--- db/migrate/20220910201747_migrate_players.rb
+++ db/migrate/20220910201747_migrate_players.rb
@@ -2,7 +2,7 @@ class MigratePlayers < ActiveRecord::Migration[7.0]
   def change
     Game.all.each do |game|
 
-      game.players_list.each do |player|
+      game.players.split("\n").each do |player|
         name = player.split(',')[0].strip
         description = nil
         description = player.split(',')[1..-1].join(',').strip if player.include?(',')
