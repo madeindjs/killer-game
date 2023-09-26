fae7e54ffd688b8c98c04372161dc2d0478aa427
madeindjs <alexandre@rsseau.fr>
2023-09-26 18:44:09 +0200

    setup daisyui

--- the-killer-online/astro.config.mjs
+++ the-killer-online/astro.config.mjs
@@ -1,10 +1,12 @@
 import nodejs from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: nodejs({ mode: "middleware" }),
+  integrations: [tailwind({ configFile: "./tailwind.config.js" })],
   vite: {
     server: {
       watch: {
