646a5f999e72bb5668dab8f64b73079bd6b6d37e
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-09-29 21:02:36 +0200

    remove warning

--- api/knexfile.js
+++ api/knexfile.js
@@ -6,18 +6,21 @@
 export default {
   development: {
     client: "better-sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./dev.sqlite3",
     },
   },
   staging: {
     client: "better-sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./staging.sqlite3",
     },
   },
   production: {
     client: "better-sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./staging.sqlite3",
     },
