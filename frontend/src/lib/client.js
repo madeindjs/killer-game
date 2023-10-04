fe4b2b06a52b732dd6d20f9bf8d781c11051dcff
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-10-05 00:17:42 +0200

    add use game listener

--- frontend/src/lib/client.js
+++ frontend/src/lib/client.js
@@ -76,3 +76,43 @@ export async function fetchPlayers(gameId, privateToken = undefined) {
   const { data } = await res.json();
   return data;
 }
+
+/**
+ * @typedef GameListenerCallbacks
+ * @property {(player: Player) => void} [onPlayerCreated]
+ */
+
+/**
+ * @param {string} gameId
+ * @param {GameListenerCallbacks} param0
+ * @returns {() => void} the method to unsubscribe
+ */
+export function useGameListener(gameId, { onPlayerCreated }) {
+  const evtSource = new EventSource(`http://localhost:3001/games/${gameId}/sse`);
+
+  const SubscriberEventNames = {
+    GameCreated: "GameCreated",
+    GameUpdated: "GameUpdated",
+    GameDeleted: "GameDeleted",
+    PlayerCreated: "PlayerCreated",
+    PlayerUpdated: "PlayerUpdated",
+    PlayerDeleted: "PlayerDeleted",
+  };
+
+  function onSseEvent(event) {
+    switch (event.event) {
+      case SubscriberEventNames.PlayerCreated:
+        return onPlayerCreated?.(event.payload);
+    }
+  }
+
+  evtSource.onmessage = (event) => {
+    try {
+      onSseEvent(JSON.parse(event.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return () => evtSource.close();
+}
