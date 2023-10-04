83add62ae407d5657429881cb477bbc8c4d2ee4c
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-10-05 00:40:48 +0200

    Add toast logic

--- frontend/src/components/GameDashboard.jsx
+++ frontend/src/components/GameDashboard.jsx
@@ -1,25 +1,37 @@
 "use client";
 import { GameContext, GameProvider } from "@/context/Game";
 import { PlayersContext, PlayersProvider } from "@/context/Players";
+import { ToastContext, ToastProvider } from "@/context/Toast";
 
 import { useGameListener } from "@/lib/client";
 import { useContext, useEffect } from "react";
+import Loader from "./Loader";
 import PlayerForm from "./PlayerForm";
 import PlayersTable from "./PlayersTable";
 
 function GameDashboardContent({ gameId, gamePrivateToken }) {
   const { game, loading: loadingGame } = useContext(GameContext);
-  const { players, loading: loadingPlayers, addPlayer, updatePlayers, createPlayer } = useContext(PlayersContext);
+  const { players, loading: loadingPlayers, addPlayer, createPlayer } = useContext(PlayersContext);
+  const { push: pushToast } = useContext(ToastContext);
 
-  useEffect(() => useGameListener(gameId, { onPlayerCreated: addPlayer }), [gameId]);
+  useEffect(
+    () =>
+      useGameListener(gameId, {
+        onPlayerCreated: (player) => {
+          addPlayer(player);
+          pushToast("success", "One player was added");
+        },
+      }),
+    [gameId]
+  );
 
-  if (loadingGame || !game) return <p>Loading</p>;
+  if (loadingGame || !game) return <Loader />;
 
   return (
     <>
       <h1>{game.name}</h1>
       <h2>Players</h2>
-      <PlayersTable gameId={game.id} players={players} />
+      {loadingPlayers ? <Loader /> : <PlayersTable gameId={game.id} players={players} />}
       <h3>New player</h3>
       <PlayerForm onSubmit={createPlayer} />
     </>
@@ -32,10 +44,12 @@ function GameDashboardContent({ gameId, gamePrivateToken }) {
  */
 export default function GameDashboard({ gameId, gamePrivateToken }) {
   return (
-    <GameProvider gameId={gameId} gamePrivateToken={gamePrivateToken}>
-      <PlayersProvider gameId={gameId} gamePrivateToken={gamePrivateToken}>
-        <GameDashboardContent gameId={gameId} gamePrivateToken={gamePrivateToken} />
-      </PlayersProvider>
-    </GameProvider>
+    <ToastProvider>
+      <GameProvider gameId={gameId} gamePrivateToken={gamePrivateToken}>
+        <PlayersProvider gameId={gameId} gamePrivateToken={gamePrivateToken}>
+          <GameDashboardContent gameId={gameId} gamePrivateToken={gamePrivateToken} />
+        </PlayersProvider>
+      </GameProvider>
+    </ToastProvider>
   );
 }
