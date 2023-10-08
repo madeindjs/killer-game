5e8bfdcf29531ac20deffef6bd9272c152f94d9b
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-10-08 10:08:41 +0200

    update game

--- frontend/src/containers/GameDashboard.jsx
+++ frontend/src/containers/GameDashboard.jsx
@@ -3,13 +3,13 @@
 import * as client from "@/lib/client";
 
 import Fetching from "@/components/Fetching";
+import GameStartStatus from "@/components/GameStartStatus";
 import PlayerCreateForm from "@/components/PlayerCreateForm";
 import PlayersAvatars from "@/components/PlayersAvatars";
 import PlayersCards from "@/components/PlayersCards";
 import { useGame } from "@/hooks/use-game";
 import { useGameEvents } from "@/hooks/use-game-events";
 import { useGamePlayers } from "@/hooks/use-game-players";
-import { useId } from "react";
 
 /**
  * @param {{gameId: string, gamePrivateToken?: string}} param0
@@ -18,7 +18,7 @@ import { useId } from "react";
 export default function GameDashboard({ gameId, gamePrivateToken }) {
   const { error: gameError, loading: gameLoading, game, setGame } = useGame(gameId, gamePrivateToken);
   const { players, addPlayer, deletePlayer, updatePlayer } = useGamePlayers(gameId, gamePrivateToken);
-  useGameEvents(gameId, { addPlayer, deletePlayer, updatePlayer });
+  useGameEvents(gameId, { addPlayer, deletePlayer, updatePlayer, setGame });
 
   function handlePlayerUpdate(player) {
     client.updatePlayer(gameId, player, gamePrivateToken).then(updatePlayer);
@@ -47,7 +47,7 @@ export default function GameDashboard({ gameId, gamePrivateToken }) {
       {game && (
         <>
           <h1 className="text-3xl mb-3">{game.name}</h1>
-          <GameDashboardStatus game={game} onChange={handleGameStartToggle} />
+          <GameStartStatus game={game} onChange={handleGameStartToggle} />
           <div className="flex sticky top-0 relative z-10 backdrop-blur pt-2">
             <h2 className="text-2xl mb-1 flex-grow">
               Players <span className="badge badge-secondary">{players.length}</span>
@@ -73,29 +73,5 @@ export default function GameDashboard({ gameId, gamePrivateToken }) {
         </>
       )}
     </Fetching>
-  );
-}
-
-/**
- * @param {{game: import("@killer-game/types").GameRecord, onChange: () => void}} param0
- */
-function GameDashboardStatus({ game, onChange }) {
-  const fieldId = useId();
-
-  return (
-    <div className="form-control w-64">
-      <label htmlFor={fieldId} className="label cursor-pointer">
-        <input
-          id={fieldId}
-          type="checkbox"
-          className="toggle toggle-primary"
-          checked={Boolean(game.started_at)}
-          onChange={onChange}
-        />
-        <span className="label-text">
-          {game.started_at ? "The game has started!" : "The game has not started yet. Start the game"}
-        </span>
-      </label>
-    </div>
   );
 }
