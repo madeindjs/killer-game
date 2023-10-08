281c159edc54c43857ddfefca706f454ee26b125
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-10-08 14:09:44 +0200

    improve register page

--- frontend/src/components/PlayersAvatars.jsx
+++ frontend/src/components/PlayersAvatars.jsx
@@ -1,11 +1,11 @@
 import PlayerAvatar from "./PlayerAvatar";
 
 /**
- * @param {{players: import('@killer-game/types').PlayerRecord[]}} param0
+ * @param {{players: import('@killer-game/types').PlayerRecord[], className?: string}} param0
  */
-export default function PlayersAvatars({ players }) {
+export default function PlayersAvatars({ players, className }) {
   return (
-    <div className="avatar-group -space-x-6">
+    <div className={"avatar-group -space-x-6 " + className}>
       {players.map((player) => (
         <PlayerAvatar player={player} key={player.id} size="s" />
       ))}
