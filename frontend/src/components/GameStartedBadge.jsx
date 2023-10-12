8e372cedf09c16bb12a5b78071f0658648c896b7
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-10-12 23:16:14 +0200

    fix classname

--- frontend/src/components/GameStartedBadge.jsx
+++ frontend/src/components/GameStartedBadge.jsx
@@ -3,8 +3,8 @@
  */
 export default function GameStartedBadge({ game }) {
   if (game.started_at) {
-    return <span class="badge badge-info">in progress</span>;
+    return <span className="badge badge-info">in progress</span>;
   } else {
-    return <span class="badge badge-neutral">un started</span>;
+    return <span className="badge badge-neutral">un started</span>;
   }
 }
