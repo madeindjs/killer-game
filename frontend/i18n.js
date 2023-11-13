e7135f2e36514a490143d88ae2831f8c558d0fd4
madeindjs <alexandre@rsseau.fr>
2023-11-13 12:52:57 +0100

    translate

--- frontend/i18n.js
+++ frontend/i18n.js
@@ -6,5 +6,7 @@ module.exports = {
     "/": ["common", "homepage"],
     "/about": ["common", "about"],
     "/games/[id]": ["common", "games"],
+    "/games/[id]/join": ["common", "games"],
+    "/players/[id]": ["common", "player-dashboard"],
   },
 };
