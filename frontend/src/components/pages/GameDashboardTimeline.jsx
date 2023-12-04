92ce976b8ce890a9adcb5956fe1d930293cfe3cd
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-12-04 18:50:50 +0100

    fix(frontend): fix loading timeline

--- frontend/src/components/pages/GameDashboardTimeline.jsx
+++ frontend/src/components/pages/GameDashboardTimeline.jsx
@@ -1,6 +1,6 @@
 "use client";
 import useTranslation from "next-translate/useTranslation";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useGamePlayersTable } from "../../hooks/use-game-players-table";
 import Empty from "../atoms/Empty";
 import Loader from "../atoms/Loader";
@@ -20,9 +20,11 @@ import PlayerModal from "../organisms/PlayerModal";
 export default function GameDashboardTimeline({ game, players, onPlayerDelete, onPlayerUpdate }) {
   const { t } = useTranslation("common");
   const [displayDead, setDisplayDead] = useState(false);
+  const [activePlayerId, setActivePlayerId] = useState(undefined);
+
   const { error, loading, table, load } = useGamePlayersTable(game.id, game.private_token, displayDead);
 
-  const [activePlayerId, setActivePlayerId] = useState(undefined);
+  useEffect(() => load({ displayAllPlayers: displayDead }), [game.id, load, players, displayDead]);
 
   const activePlayer = useMemo(
     () => (activePlayerId ? players.find((p) => activePlayerId === p.id) : undefined),
