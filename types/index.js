eb577bc509008d35eb0a823e1d6c5474f336aa79
Alexandre Rousseau <alexandre@rsseau.fr>
2024-06-08 13:24:41 +0200

    use only frontend

--- types/index.js
+++
@@ -1,8 +0,0 @@
-export const SubscriberEventNames = {
-  GameCreated: "GameCreated",
-  GameUpdated: "GameUpdated",
-  GameDeleted: "GameDeleted",
-  PlayerCreated: "PlayerCreated",
-  PlayerUpdated: "PlayerUpdated",
-  PlayerDeleted: "PlayerDeleted",
-};
