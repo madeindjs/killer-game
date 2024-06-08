b1811986c9e7a6781d317dc4b6d99e067962311a
Alexandre Rousseau <alexandre@rsseau.fr>
2024-06-09 01:18:08 +0200

    chore: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 cd ~/killer-game/
 
 # API
-docker pull arousseau/killer-game-api
 docker stop "$(docker ps -q --filter ancestor=arousseau/killer-game-api)"
+docker pull arousseau/killer-game-api
 docker run -p 3001:3001 -d -v "$PWD/db:/app/api/db" arousseau/killer-game-api
 
 # frontend
-docker pull arousseau/killer-game-frontend
 docker stop "$(docker ps -q --filter ancestor=arousseau/killer-game-frontend)"
+docker pull arousseau/killer-game-frontend
 docker run -p 3000:3000 -d arousseau/killer-game-frontend
