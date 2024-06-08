0708c52c0dfc0b49ddf8d912e35615922a825cef
Alexandre Rousseau <alexandre@rsseau.fr>
2024-06-08 22:59:34 +0200

    fix: follow locale on game creation

--- frontend/src/components/pages/GameCreateForm.jsx
+++ frontend/src/components/pages/GameCreateForm.jsx
@@ -29,7 +29,7 @@ export default function GameCreateForm() {
       .createGame(game)
       .then((game) => {
         addGame(game);
-        router.push(getGameUrl(game), lang);
+        router.push(getGameUrl(game, lang));
       })
       .finally(() => setBusy(false));
   }
