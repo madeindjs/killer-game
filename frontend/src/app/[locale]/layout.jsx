f448024bef874b076b556ff659dc199d04be4f24
Alexandre Rousseau <alexandre@rsseau.fr>
2024-06-10 21:14:20 +0200

    fix favicon

--- frontend/src/app/[locale]/layout.jsx
+++ frontend/src/app/[locale]/layout.jsx
@@ -1,6 +1,5 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import Head from "next/head";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
@@ -11,12 +10,19 @@ export default async function RootLayout({ children, params: { locale } }) {
 
   return (
     <html lang={locale}>
-      <Head>
+      <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="mask-icon" href="/favicon.svg" color="#000000"></link>
-      </Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="mask-icon" href="/favicon/favicon.svg" color="#000000"></link>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>
         <main className="flex flex-col" style={{ minHeight: "100vh" }}>
           <NavBar />
